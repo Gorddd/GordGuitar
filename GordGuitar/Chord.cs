7e421969c1ba0728b640a3629847b3f08c432526
Gorddd <kasual2014@icloud.com>
2022-01-13 23:29:34 +0300

    start work with classes Chord and GuitarString

---
+++ GordGuitar/Chord.cs
@@ -0,0 +1,18 @@
+﻿namespace GordGuitar
+{
+    class Chord
+    {
+        public Chord()
+        {
+            for (int i = 0; i < guitarStrings.Length; i++)
+            {
+                guitarStrings[i] = new GuitarString();
+            }
+        }
+
+        /// <summary>
+        /// 6 strings of the chord
+        /// </summary>
+        public GuitarString[] guitarStrings { get; set; } = new GuitarString[6];
+    }
+}
---
+++ GordGuitar/GuitarString.cs
@@ -0,0 +1,34 @@
+﻿using WMPLib;
+
+namespace GordGuitar
+{
+    class GuitarString
+    {
+        public GuitarString()
+        {
+            mediaPlayer.URL = "mute.wav";
+        }
+
+        /// <summary>
+        /// Play sound
+        /// </summary>
+        public void Play()
+        {
+            mediaPlayer.controls.stop();
+            mediaPlayer.controls.play();
+        }
+
+        /// <summary>
+        /// Set source path
+        /// </summary>
+        public string URL
+        {
+            set { mediaPlayer.URL = value; }
+        }
+
+        /// <summary>
+        /// Media player to play the sound
+        /// </summary>
+        private WindowsMediaPlayer mediaPlayer;
+    }
+}
