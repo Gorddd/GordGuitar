918133a2c8d4046a5400d9e44703bec6077e72ee
Gorddd <kasual2014@icloud.com>
2022-01-30 18:09:41 +0300

    add design to newChordform

--- GordGuitar/CreateNewChordForm.Designer.cs
+++ GordGuitar/CreateNewChordForm.Designer.cs
@@ -30,59 +30,66 @@ namespace GordGuitar
         private void InitializeComponent()
         {
             this.nameBox = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.cancelButton = new System.Windows.Forms.Button();
             this.applyButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // nameBox
             // 
-            this.nameBox.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.nameBox.Location = new System.Drawing.Point(151, 31);
+            this.nameBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameBox.Location = new System.Drawing.Point(152, 12);
             this.nameBox.Name = "nameBox";
-            this.nameBox.Size = new System.Drawing.Size(218, 32);
+            this.nameBox.Size = new System.Drawing.Size(230, 35);
             this.nameBox.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(24, 31);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(121, 29);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Chord name";
             // 
             // cancelButton
             // 
+            this.cancelButton.BackColor = System.Drawing.Color.Coral;
             this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.cancelButton.Location = new System.Drawing.Point(57, 88);
+            this.cancelButton.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cancelButton.Location = new System.Drawing.Point(64, 63);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(130, 29);
             this.cancelButton.TabIndex = 6;
             this.cancelButton.Text = "Cancel";
-            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.UseVisualStyleBackColor = false;
             this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
             // applyButton
             // 
+            this.applyButton.BackColor = System.Drawing.Color.Coral;
             this.applyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.applyButton.Location = new System.Drawing.Point(193, 88);
+            this.applyButton.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.applyButton.Location = new System.Drawing.Point(200, 63);
             this.applyButton.Name = "applyButton";
             this.applyButton.Size = new System.Drawing.Size(130, 29);
             this.applyButton.TabIndex = 7;
             this.applyButton.Text = "Apply";
-            this.applyButton.UseVisualStyleBackColor = true;
+            this.applyButton.UseVisualStyleBackColor = false;
             this.applyButton.Click += new System.EventHandler(this.applyButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 30);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Chord name";
             // 
             // NewChordForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(405, 133);
+            this.BackgroundImage = global::GordGuitar.Properties.Resources.ChordOptionsBack;
+            this.ClientSize = new System.Drawing.Size(394, 104);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.applyButton);
             this.Controls.Add(this.cancelButton);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.nameBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -97,8 +104,8 @@ namespace GordGuitar
         #endregion
 
         private System.Windows.Forms.TextBox nameBox;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Button applyButton;
+        private System.Windows.Forms.Label label2;
     }
 }
