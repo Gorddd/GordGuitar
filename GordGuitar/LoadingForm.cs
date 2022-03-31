ca9d597fa71bbe7a55e70f58756121a69a7d6cba
Gorddd <kasual2014@icloud.com>
2022-03-31 17:38:21 +0300

    add loadingform and show gameform after loading

---
+++ GordGuitar/LoadingForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GordGuitar
+{
+    public partial class LoadingForm : Form
+    {
+        public LoadingForm()
+        {
+            InitializeComponent();
+
+            new Thread(() => 
+            { 
+                GameForm gameForm = new GameForm();
+                Hide();
+                gameForm.ShowDialog();
+                Close();
+            }).Start();
+        }
+    }
+}
--- GordGuitar/Program.cs
+++ GordGuitar/Program.cs
@@ -16,7 +16,7 @@ namespace GordGuitar
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GameForm());
+            Application.Run(new LoadingForm());
         }
     }
 }
